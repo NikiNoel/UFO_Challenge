694feed487c8dba4af26314e5acbfc507c838862
Niki <NikiNoel1218@gmail.com>
2022-09-01 11:36:17 -0400

    Web page updated with background

--- Static/js/app.js
+++ Static/js/app.js
@@ -1,5 +1,6 @@
 // from data.js
 const tableData = data;
+console.log(tableData);
 
 // get table references
 var tbody = d3.select("tbody");
@@ -22,6 +23,26 @@ function buildTable(data) {
     });
   });
 }
+
+function handleClick() {
+  // Grab the datetime value from the filter
+  let date = d3.select("#datetime").property("value");
+  let filteredData = tableData;
+
+   // Check to see if a date was entered and filter the
+  // data using that date.
+  if (date) {
+    // Apply `filter` to the table data to only keep the
+    // rows where the `datetime` value matches the filter value
+    filteredData = filteredData.filter(row => row.datetime === date);
+  };
+
+   // Rebuild the table using the filtered data
+  // @NOTE: If no date was entered, then filteredData will
+  // just be the original tableData.
+  buildTable(filteredData);
+};
+
 
 // 1. Create a variable to keep track of all the filters as an object.
 
@@ -60,7 +81,8 @@ function updateFilters() {
   }
   
   // 2. Attach an event to listen for changes to each filter
-  
+  d3.selectAll("#filter-btn").on("click", handleClick);
+
   
   // Build the table when the page loads
   buildTable(tableData);
